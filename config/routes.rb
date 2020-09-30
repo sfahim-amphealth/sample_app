22fc15f64f47ef8d3addd32be53e2294a0acbcee
sfahim-amphealth <sfahim@adaptivemedicalpartners.com>
2020-09-30 17:20:32 -0500

    Added controller location to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   get 'static_pages/home' => "static_pages#home"
-  get 'static_pages/help'  => "static_pages#home"
-  get 'static_pages/about'  => "static_pages#home"
-  get 'static_pages/contact'  => "static_pages#home"
+  get 'static_pages/help'  => "static_pages#help"
+  get 'static_pages/about'  => "static_pages#about"
+  get 'static_pages/contact'  => "static_pages#contact"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'application#hello'
 end
