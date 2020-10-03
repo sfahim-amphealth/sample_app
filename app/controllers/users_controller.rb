1fdeb1f412fc7b27fdec53aa4ae06a1b775234f5
sfahim-amphealth <sfahim@adaptivemedicalpartners.com>
2020-10-03 15:21:52 -0500

    Complete new User signup and save to db

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,14 +11,15 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # handle a successful save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else 
       render 'new'
     end
   end
 
   private
-  
+
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
