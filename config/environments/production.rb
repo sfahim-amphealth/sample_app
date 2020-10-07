47ced776cec72d04a3c8bc2fb1e3c91525885620
sfahim-amphealth <sfahim@adaptivemedicalpartners.com>
2020-10-07 10:15:02 -0500

    Add sendgrid configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,14 +67,14 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = 'sample-slrfhm.herokuapp.com/'
-  config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
+    :domain         => 'sample-slrfhm.herokuapp.com/',
     :enable_starttls_auto => true
   }
 
